541ef5b53eee41a4abda35337a0d9f448109b2c1
Yar0s1av-O <yaroslav.oleinychenko@student.nhlstenden.com>
2025-04-06 01:41:15 +0200

    command invoker tests

--- src/test/java/com/mycompany/commands/CommandInvokerTest.java
+++ src/test/java/com/mycompany/commands/CommandInvokerTest.java
@@ -2,6 +2,8 @@ package com.mycompany.commands;
 
 import org.junit.jupiter.api.Test;
 
+import java.awt.*;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CommandInvokerTest
@@ -10,10 +12,20 @@ class CommandInvokerTest
     @Test
     void register()
     {
+        Command command = new ExitCommand();
+
+        CommandInvoker commandInvoker = new CommandInvoker();
+        commandInvoker.register("exit", command);
+
+        assertEquals(1, commandInvoker.getCommandMap().size());
     }
 
+
     @Test
-    void execute()
+    void execute_nonexitentCommand_noException()
     {
+        CommandInvoker commandInvoker = new CommandInvoker();
+
+        assertDoesNotThrow(()->{commandInvoker.execute("show");});
     }
 }
