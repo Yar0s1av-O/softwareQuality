93076c293b5c081c02e3972fed3fb6d8b7ac6511
Yar0s1av-O <yaroslav.oleinychenko@student.nhlstenden.com>
2025-04-05 19:29:23 +0200

    new methods for less levels

---
+++ src/test/java/com/mycompany/commands/CommandInvokerTest.java
@@ -0,0 +1,19 @@
+package com.mycompany.commands;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CommandInvokerTest
+{
+
+    @Test
+    void register()
+    {
+    }
+
+    @Test
+    void execute()
+    {
+    }
+}
